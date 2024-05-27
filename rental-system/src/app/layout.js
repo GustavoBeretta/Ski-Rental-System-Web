d66565a8b6123ac690f6b23142fc804f7a1ade0a
Josef Bruseke <josefbruseke@gmail.com>
2024-05-27 11:24:26 -0300

    change deafult font

--- rental-system/src/app/layout.js
+++ rental-system/src/app/layout.js
@@ -1,8 +1,11 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ 
+    weight: "500", 
+    subsets: ["latin"] 
+});
 
 export const metadata = {
   title: "Rental System",
@@ -12,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <NavBar/>
         {children}
         </body>
