3a40ac77eab3664dfe4e0b610825e8ed35c63d71
Luiz Adriano <santosluiz1912@gmail.com>
2024-06-22 10:25:26 -0300

    Atualizacao pagina Login

--- rental-system/src/app/page.js
+++ rental-system/src/app/page.js
@@ -1,36 +1,41 @@
-export default function Login() { 
+'use client'; // Isso marca o componente como um Client Component
+
+import React, { useState } from 'react';
+
+export default function Login() {
+    const [isEmployeeLogin, setIsEmployeeLogin] = useState(false);
+
+    const handleEmployeeLoginClick = () => {
+        setIsEmployeeLogin(true);
+    };
+
     return (
         <main>
             <section>
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <h1 className="text-5xl text-[#8F8E8E]">Login</h1>
-                    <p className="text-l text-[#8F8E8E] mt-4">Sign in to continue</p>
-                    <div className="w-full bg-white rounded-lg md:mt-0 sm:max-w-md xl:p-0"> 
+                <div className="flex flex-col items-center justify-center px-6 py-8 mt-52 lg:py-0">
+                    <h1 id="title" className="text-5xl text-[#8F8E8E] mb-5">
+                        {isEmployeeLogin ? 'Employee Login' : 'Guest Login'}
+                    </h1>
+                    <div className="w-full bg-white rounded-lg md:mt-0 sm:max-w-md xl:p-0">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-[#8F8E8E]">Your email</label>
-                                    <input type="email" name="email" id="email" className="bg-[#ECECEC] border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""/>
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-[#8F8E8E]">EMAIL</label>
+                                    <input type="email" name="email" id="email" className="bg-[#ECECEC] border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
                                 </div>
                                 <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-[#8F8E8E]">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-[#ECECEC] border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""/>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-[#8F8E8E]">PASSWORD</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-[#ECECEC] border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
                                 </div>
-                                <div className="flex items-center justify-between">
-                                    <div className="flex items-start">
-                                        <div className="flex items-center h-5">
-                                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                                        </div>
-                                        <div className="ml-3 text-sm">
-                                            <label htmlFor="remember" className="text-[#8F8E8E]">Remember me</label>
-                                        </div>
-                                    </div>
-                                    <a href="#" className="text-sm font-medium text-[#8F8E8E] hover:underline hover:text-black">Forgot password?</a>
+                                <div className="flex items-center justify-center flex-col">
+                                    {!isEmployeeLogin ? (
+                                        <a href="./cadastro" className="text-sm font-medium text-[#8F8E8E] hover:underline hover:text-black">Create new account</a>
+                                    ) : (<a href="/" className="text-sm font-medium text-[#8F8E8E] hover:underline hover:text-black">Guest? Login here</a>)}
+                                    {!isEmployeeLogin && (
+                                        <a href="#" id="employeeBtton" className="text-sm font-medium text-[#8F8E8E] hover:underline hover:text-black mt-3" onClick={handleEmployeeLoginClick}>Employee? Login here</a>
+                                    )}
                                 </div>
                                 <button type="submit" className="w-full text-white bg-[#4094A5] hover:bg-[#81C9D8] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <a href="./cadastro" className="font-medium text-primary-600 hover:underline hover:text-black">Sign up</a>
-                                </p>
                             </form>
                         </div>
                     </div>
