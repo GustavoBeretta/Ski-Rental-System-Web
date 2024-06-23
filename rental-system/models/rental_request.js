dc9da63ee6e4589c090a5b79d0d15535727f26c9
AndresKindel <andreskbarbosa@outlook.com>
2024-06-23 18:44:30 -0300

    feat: Update rental request model and API endpoints
    This commit updates the rental request model and API endpoints to include additional fields such as `userId`, `sport`, `date`, `time`, `ski_board`, `boots`, and `helmet`. These changes enhance the functionality of the rental-system application by allowing users to specify their preferences for rental requests.

--- rental-system/models/rental_request.js
+++ rental-system/models/rental_request.js
@@ -2,7 +2,14 @@ import mongoose, {Schema} from "mongoose";
 
 const rental_requestSchema = new Schema(
     {
+        userId: String,
+        sport: String,
+        date: Date,
+        time: String,
         status: String,
+        ski_board: Boolean,
+        boots: Boolean,
+        helmet: Boolean
     },
     {
         timestamps: true,
--- rental-system/src/app/api/rental-requests/route.js
+++ rental-system/src/app/api/rental-requests/route.js
@@ -3,9 +3,9 @@ import connectMongoDB from "../../../../libs/mongodb"
 import RentalRequest from "../../../../models/rental_request";
 
 export async function POST(request) {
-    const {status} = await request.json();
+    const { userId, sport, date, time, status, ski_board, boots, helmet } = await request.json();
     await connectMongoDB();
-    await RentalRequest.create({status});
+    await RentalRequest.create({ userId, sport, date, time, status, ski_board, boots, helmet });
     return NextResponse.json({message: "Rental Request created"}, {status: 201})
 
 }
