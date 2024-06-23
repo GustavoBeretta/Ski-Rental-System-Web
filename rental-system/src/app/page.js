7a060e0b10ff06129c68a57986dde150f0f51a6f
AndresKindel <andreskbarbosa@outlook.com>
2024-06-23 15:50:12 -0300

    feat: Add user login functionality
    This commit adds user login functionality to the Login component in the rental-system application. It includes the implementation of the findUser function to fetch user data from the API, the onSubmit function to handle form submission, and the validation of user credentials using bcrypt. Additionally, it handles different login scenarios based on the user's role. This change improves the user experience by allowing users to securely log in to the application.

--- rental-system/src/app/page.js
+++ rental-system/src/app/page.js
@@ -1,9 +1,57 @@
-'use client'; // Isso marca o componente como um Client Component
+'use client';
 
+import bcrypt from 'bcryptjs';
 import React, { useState } from 'react';
 
 export default function Login() {
     const [isEmployeeLogin, setIsEmployeeLogin] = useState(false);
+
+    async function findUser(email) {
+        const response = await fetch("http://localhost:3000/api/users");
+        const usuarios = await response.json();
+        const user = await usuarios.users.find((usuario) => usuario.email === email);
+        return user;
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        const user = await findUser(email);
+
+        if (!user) {    
+            window.alert("User not found");
+            return;
+        }
+
+        bcrypt.compare(password, user.password, (err, res) => {
+            if (err) {
+                window.alert("Error");
+                return;
+            }
+
+            if (!res) {
+                window.alert("Incorrect password");
+                return;
+            }
+        });
+
+        if (isEmployeeLogin) {
+            if (user.role !== "employee") {
+                window.alert("User is not an employee");
+                return;
+            }
+        }
+
+        if (!isEmployeeLogin) {
+            if (user.role === "employee") {
+                window.alert("Employee cannot login here");
+                return;
+            }
+        }
+    }
 
     const handleEmployeeLoginClick = () => {
         setIsEmployeeLogin(true);
@@ -18,7 +66,7 @@ export default function Login() {
                     </h1>
                     <div className="w-full bg-white rounded-lg md:mt-0 sm:max-w-md xl:p-0">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <form className="space-y-4 md:space-y-6" action="#">
+                            <form className="space-y-4 md:space-y-6" action="#" onSubmit={onSubmit}>
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-[#8F8E8E]">EMAIL</label>
                                     <input type="email" name="email" id="email" className="bg-[#ECECEC] border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
