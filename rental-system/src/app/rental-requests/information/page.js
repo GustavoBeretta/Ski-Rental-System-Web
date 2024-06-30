51470d0e5581e45da49b39006ab9739d71359684
AndresKindel <andreskbarbosa@outlook.com>
2024-06-30 16:10:12 -0300

    refactor: Restructure user rental request page

--- rental-system/src/app/rental-requests/information/page.js
+++ rental-system/src/app/rental-requests/information/page.js
@@ -4,8 +4,8 @@ import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
 export default function RentalRequestInformation() {
-    const [userData, setUserData] = useState(null);
-    const [buttonText, setButtonText] = useState("teste");
+    const [rentalRequestData, setRentalRequestData] = useState(null);
+    const [buttonText, setButtonText] = useState("Cancel");
 
     const router = useRouter();
     const currentUrl = window.location.href;
@@ -14,7 +14,7 @@ export default function RentalRequestInformation() {
     const id = params.get('id');
 
     useEffect(() => {
-        const fetchUserData = async () => {
+        const fetchRentalRequestData = async () => {
             try {
                 const res = await fetch(`http://localhost:3000/api/rental-requests/${id}`, {
                     cache: "no-store"
@@ -24,13 +24,13 @@ export default function RentalRequestInformation() {
                 }
                 const data = await res.json();
                 console.log(data.rentalRequest);
-                setUserData(data.rentalRequest);
+                setRentalRequestData(data.rentalRequest);
             } catch (error) {
                 console.log("Error loading user data: ", error);
             }
         };
 
-        fetchUserData();
+        fetchRentalRequestData();
     }, []);
 
     const formatDate = (timestamp) => {
@@ -49,21 +49,21 @@ export default function RentalRequestInformation() {
     };
 
     const inputs = [
-        { label: "Nome completo", name: "name", type: "text" },
-        { label: "Esporte", name: "sport", type: "text" },
-        { label: "Data", name: "date", type: "text" },
-        { label: "Hora", name: "time", type: "text" },
+        { label: "Full Name", name: "nameUser", type: "text" },
+        { label: "Sport", name: "sport", type: "text" },
+        { label: "Date", name: "date", type: "text" },
+        { label: "Time", name: "time", type: "text" },
         { label: "Status", name: "status", type: "text" },
-        { label: "Gênero", name: "gender", type: "select", options: ["Masculino", "Feminino"] },
-        { label: "Tamanho de sapato (EUA)", name: "shoeSize", type: "number" },
-        { label: "Idade", name: "age", type: "number" },
-        { label: "Peso (KG)", name: "weight", type: "number" },
+        { label: "Gender", name: "gender", type: "select", options: ["Male", "Female"] },
+        { label: "Shoe size", name: "shoeSize", type: "number" },
+        { label: "Age", name: "age", type: "number" },
+        { label: "Weight (KG)", name: "weight", type: "number" },
     ];
 
     return (
         <main>
             <section className="flex flex-col items-center justify-center snap-none">
-                <h1 className="lg:text-4xl text-2xl text-[#8F8E8E] pt-6">Informações da Solicitação de Aluguel</h1>
+                <h1 className="lg:text-4xl text-2xl text-[#8F8E8E] pt-6">Rental Request Information</h1>
                 <div className="flex items-center content-center flex-col lg:w-6/12 ">
                     <form className="w-full rounded-lg space-y-4 sm:p-8">
                         {inputs.map((input) => (
@@ -89,10 +89,10 @@ export default function RentalRequestInformation() {
                                         name={input.name}
                                         id={input.name}
                                         defaultValue={
-                                            userData ? 
-                                            (input.name === 'date' ? formatDate(userData.createdAt) :
-                                             input.name === 'time' ? formatTime(userData.createdAt) :
-                                             userData[input.name]) 
+                                            rentalRequestData ? 
+                                            (input.name === 'date' ? formatDate(rentalRequestData.createdAt) :
+                                             input.name === 'time' ? formatTime(rentalRequestData.createdAt) :
+                                             rentalRequestData[input.name]) 
                                             : ''
                                         }
                                         className="bg-[#ECECEC] border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
@@ -102,11 +102,11 @@ export default function RentalRequestInformation() {
                             </div>
                         ))}
                         <div className="flex flex-col space-y-4">
-                            <div className="text-sm font-medium text-[#8F8E8E]">Inclui</div>
+                            <div className="text-sm font-medium text-[#8F8E8E]">INCLUDES :</div>
                             <div className="flex items-center">
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
-                                        <input id="ski-board" aria-describedby="ski-board" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" disabled />
+                                        <input id="ski-board" checked={rentalRequestData?.ski_board} aria-describedby="ski-board" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" disabled />
                                     </div>
                                     <div className="ml-3 text-sm">
                                         <label htmlFor="ski-board" className="text-[#8F8E8E]">SKI/BOARD</label>
@@ -116,20 +116,20 @@ export default function RentalRequestInformation() {
                             <div className="flex items-center">
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
-                                        <input id="boots" aria-describedby="boots" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" disabled />
+                                        <input id="boots" checked={rentalRequestData?.boots} aria-describedby="boots" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" disabled />
                                     </div>
                                     <div className="ml-3 text-sm">
-                                        <label htmlFor="boots" className="text-[#8F8E8E]">BOTAS</label>
+                                        <label htmlFor="boots" className="text-[#8F8E8E]">BOOTS</label>
                                     </div>
                                 </div>
                             </div>
                             <div className="flex items-center">
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
-                                        <input id="helmet" aria-describedby="helmet" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" disabled />
+                                        <input id="helmet" checked={rentalRequestData?.helmet} aria-describedby="helmet" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" disabled />
                                     </div>
                                     <div className="ml-3 text-sm">
-                                        <label htmlFor="helmet" className="text-[#8F8E8E]">CAPACETE</label>
+                                        <label htmlFor="helmet" className="text-[#8F8E8E]">HELMET</label>
                                     </div>
                                 </div>
                             </div>
