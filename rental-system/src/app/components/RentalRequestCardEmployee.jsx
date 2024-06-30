e098e611633a4ea578672459357cac28a731f51e
Josef Bruseke <josefbruseke@gmail.com>
2024-06-30 17:08:12 -0300

    change design

--- rental-system/src/app/components/RentalRequestCardEmployee.jsx
+++ rental-system/src/app/components/RentalRequestCardEmployee.jsx
@@ -1,7 +1,7 @@
-const RentalRequestCardEmployee = ({user, status, time, date, sport}) => {
+const RentalRequestCardEmployee = ({ user, status, time, date, sport }) => {
 
     return (
-        <button className="rounded-lg bg-gray-200 m-2">
+        <button className="rounded-lg bg-gray-200 m-2 w-full">
             <div className="bg-sky-200 rounded-lg p-2">
                 <p>{date} at {time}</p>
             </div>
