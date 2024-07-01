d1c782e64ea86fa3c70223d4df32e3daf9e2c631
Josef Bruseke <josefbruseke@gmail.com>
2024-07-01 11:28:59 -0300

    add change

--- rental-system/src/middleware.js
+++ rental-system/src/middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/home", "/edit-account", "/rental-requests", "/homeEmployee", "/rental-requests-employee", "/usersRegistered", "/edit-account-employee", "/cadastro"] }
+export const config = { matcher: ["/home", "/edit-account", "/rental-requests", "/homeEmployee", "/rental-requests-employee", "/usersRegistered", "/edit-account-employee",] }
