a2643dea2ab29d511d4ed61e0280b8d9735c43d4
Josef Bruseke <josefbruseke@gmail.com>
2024-07-01 08:46:27 -0300

    change alert window

--- rental-system/src/app/home/page.js
+++ rental-system/src/app/home/page.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from 'react';
 import NavBar from "../components/NavBar";
+import Swal from 'sweetalert2';
 
 export default function Home() {
   const router = useRouter();
@@ -39,7 +40,7 @@ export default function Home() {
     const helmet = document.getElementById('helmet').checked;
 
     if (!ski_board && !boots && !helmet) {
-      alert('Please select at least one item to rent');
+      Swal.fire('Please select at least one item to rent!', '', 'warning')
       return;
     }
 
@@ -72,7 +73,7 @@ export default function Home() {
         throw new Error("Failed to fetch the rental requests")
       }
 
-      alert('Rental request submitted successfully!');
+      Swal.fire('Rental request submitted successfully!', '', 'success')
     } catch (error) {
       console.log("Error loading topics: ", error)
     }
